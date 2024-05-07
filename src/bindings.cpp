5fc8ac878ac459ee9f519f126ff484f255dc09c4
Sam Schoedel <seschoedel@gmail.com>
2024-05-07 15:58:08 -0400

    Add wrapper and bindings for setting x_ref and u_ref

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -14,10 +14,14 @@ class PyTinySolver {
         PyTinySolver(Eigen::Ref<tinyMatrix>, Eigen::Ref<tinyMatrix>, // A, B
                      Eigen::Ref<tinyMatrix>, Eigen::Ref<tinyMatrix>, // Q, R
                      float, int, int, int,               // rho, nx, nu, N
-                     Eigen::Ref<tinyVector>, Eigen::Ref<tinyVector>, // x_min, x_max
-                     Eigen::Ref<tinyVector>, Eigen::Ref<tinyVector>, // u_min, u_max
-                     TinySettings*); // settings
+                     Eigen::Ref<tinyMatrix>, Eigen::Ref<tinyMatrix>, // x_min, x_max
+                     Eigen::Ref<tinyMatrix>, Eigen::Ref<tinyMatrix>, // u_min, u_max
+                     TinySettings*, int verbose); // settings, verbosity
+        void set_x0(Eigen::Ref<tinyVector>);
+        void set_x_ref(Eigen::Ref<tinyMatrix>);
+        void set_u_ref(Eigen::Ref<tinyMatrix>);
         int solve();
+        void print_problem_data();
         TinySolution* get_solution();
     private:
         TinySolver *_solver;
@@ -32,19 +36,23 @@ PyTinySolver::PyTinySolver(
         int nx,
         int nu,
         int N,
-        Eigen::Ref<tinyVector> x_min,
-        Eigen::Ref<tinyVector> x_max,
-        Eigen::Ref<tinyVector> u_min,
-        Eigen::Ref<tinyVector> u_max,
-        TinySettings *settings) {
+        Eigen::Ref<tinyMatrix> x_min,
+        Eigen::Ref<tinyMatrix> x_max,
+        Eigen::Ref<tinyMatrix> u_min,
+        Eigen::Ref<tinyMatrix> u_max,
+        TinySettings *settings,
+        int verbose) {
+            
     TinySolution *solution = new TinySolution();
     TinyCache *cache = new TinyCache();
     TinyWorkspace *work = new TinyWorkspace();
-    int status = tiny_setup(cache, work, solution, A, B, Q, R, rho, nx, nu, N, x_min, x_max, u_min, u_max, settings);
+
+    int status = tiny_setup(cache, work, solution, A, B, Q, R, rho, nx, nu, N, x_min, x_max, u_min, u_max, settings, verbose);
     if (status) {
         std::string message = "Error during setup";
         throw py::value_error(message); 
     }
+
     this->_solver = new TinySolver();
     this->_solver->solution = solution;
     this->_solver->settings = settings;
@@ -52,19 +60,93 @@ PyTinySolver::PyTinySolver(
     this->_solver->work = work;
 }
 
+void PyTinySolver::set_x0(Eigen::Ref<tinyVector> x0) {
+    tiny_set_x0(this->_solver, x0);
+}
+
+void PyTinySolver::set_x_ref(Eigen::Ref<tinyMatrix> x_ref) {
+    tiny_set_x_ref(this->_solver, x_ref);
+}
+
+void PyTinySolver::set_u_ref(Eigen::Ref<tinyMatrix> u_ref) {
+    tiny_set_u_ref(this->_solver, u_ref);
+}
+
 int PyTinySolver::solve() {
     py::gil_scoped_release release;
     int status = tiny_solve(this->_solver);
     py::gil_scoped_acquire acquire;
-    return status;
+    return 0;
 }
 
 TinySolution* PyTinySolver::get_solution() {
     return this->_solver->solution;
 }
 
+void PyTinySolver::print_problem_data() {
+    std::cout << "solution iter:\n" << this->_solver->solution->iter << std::endl;
+    std::cout << "solution solved:\n" << this->_solver->solution->solved << std::endl;
+    std::cout << "solution x:\n" << this->_solver->solution->x << std::endl;
+    std::cout << "solution u:\n" << this->_solver->solution->u << std::endl;
+
+    std::cout << "\n\n\ncache rho: " << this->_solver->cache->rho << std::endl;
+    std::cout << "cache Kinf:\n" << this->_solver->cache->Kinf << std::endl;
+    std::cout << "cache Pinf:\n" << this->_solver->cache->Pinf << std::endl;
+    std::cout << "cache Quu_inv:\n" << this->_solver->cache->Quu_inv << std::endl;
+    std::cout << "cache AmBKt:\n" << this->_solver->cache->AmBKt << std::endl;
+
+    std::cout << "\n\n\nabs_pri_tol: " << this->_solver->settings->abs_pri_tol << std::endl;
+    std::cout << "abs_dua_tol: " << this->_solver->settings->abs_dua_tol << std::endl;
+    std::cout << "max_iter: " << this->_solver->settings->max_iter << std::endl;
+    std::cout << "check_termination: " << this->_solver->settings->check_termination << std::endl;
+    std::cout << "en_state_bound: " << this->_solver->settings->en_state_bound << std::endl;
+    std::cout << "en_input_bound: " << this->_solver->settings->en_input_bound << std::endl;
+
+    std::cout << "\n\n\nnx: " << this->_solver->work->nx << std::endl;
+    std::cout << "nu: " << this->_solver->work->nu << std::endl;
+    std::cout << "x:\n" << this->_solver->work->x << std::endl;
+    std::cout << "u:\n" << this->_solver->work->u << std::endl;
+    std::cout << "q:\n" << this->_solver->work->q << std::endl;
+    std::cout << "r:\n" << this->_solver->work->r << std::endl;
+    std::cout << "p:\n" << this->_solver->work->p << std::endl;
+    std::cout << "d:\n" << this->_solver->work->d << std::endl;
+    std::cout << "v:\n" << this->_solver->work->v << std::endl;
+    std::cout << "vnew:\n" << this->_solver->work->vnew << std::endl;
+    std::cout << "z:\n" << this->_solver->work->z << std::endl;
+    std::cout << "znew:\n" << this->_solver->work->znew << std::endl;
+    std::cout << "g:\n" << this->_solver->work->g << std::endl;
+    std::cout << "y:\n" << this->_solver->work->y << std::endl;
+    std::cout << "Q:\n" << this->_solver->work->Q << std::endl;
+    std::cout << "R:\n" << this->_solver->work->R << std::endl;
+    std::cout << "Adyn:\n" << this->_solver->work->Adyn << std::endl;
+    std::cout << "Bdyn:\n" << this->_solver->work->Bdyn << std::endl;
+    std::cout << "x_min:\n" << this->_solver->work->x_min << std::endl;
+    std::cout << "x_max:\n" << this->_solver->work->x_max << std::endl;
+    std::cout << "u_min:\n" << this->_solver->work->u_min << std::endl;
+    std::cout << "u_max:\n" << this->_solver->work->u_max << std::endl;
+    std::cout << "Xref:\n" << this->_solver->work->Xref << std::endl;
+    std::cout << "Uref:\n" << this->_solver->work->Uref << std::endl;
+    std::cout << "Qu:\n" << this->_solver->work->Qu << std::endl;
+    std::cout << "primal_residual_state:\n" << this->_solver->work->primal_residual_state << std::endl;
+    std::cout << "primal_residual_input:\n" << this->_solver->work->primal_residual_input << std::endl;
+    std::cout << "dual_residual_state:\n" << this->_solver->work->dual_residual_state << std::endl;
+    std::cout << "dual_residual_input:\n" << this->_solver->work->dual_residual_input << std::endl;
+    std::cout << "status:\n" << this->_solver->work->status << std::endl;
+    std::cout << "iter:\n" << this->_solver->work->iter << std::endl;
+}
+
 PYBIND11_MODULE(ext_tinympc, m) {
 // // PYBIND11_MODULE(@TINYMPC_EXT_MODULE_NAME@, m) {
+
+    // Solution
+    py::class_<TinySolution>(m, "TinySolution", py::module_local())
+    .def(py::init([]() {
+        return new TinySolution();
+    }))
+    .def_readwrite("iter", &TinySolution::iter)
+    .def_readwrite("solved", &TinySolution::solved)
+    .def_readwrite("x", &TinySolution::x)
+    .def_readwrite("u", &TinySolution::u);
 
     // Cache
     py::class_<TinyCache>(m, "TinyCache", py::module_local())
@@ -93,9 +175,13 @@ PYBIND11_MODULE(ext_tinympc, m) {
 
     // Solver
     py::class_<PyTinySolver>(m, "TinySolver", py::module_local())
-    .def(py::init<Eigen::Ref<tinyMatrix>, Eigen::Ref<tinyMatrix>, Eigen::Ref<tinyMatrix>, Eigen::Ref<tinyMatrix>, float, int, int, int, Eigen::Ref<tinyVector>, Eigen::Ref<tinyVector>, Eigen::Ref<tinyVector>, Eigen::Ref<tinyVector>, TinySettings*>(),
-            "A"_a.noconvert(), "B"_a.noconvert(), "Q"_a.noconvert(), "R"_a.noconvert(), "rho"_a, "nx"_a, "nu"_a, "N"_a, "x_min"_a.noconvert(), "x_max"_a.noconvert(), "u_min"_a.noconvert(), "u_max"_a.noconvert(), "settings"_a)
+    .def(py::init<Eigen::Ref<tinyMatrix>, Eigen::Ref<tinyMatrix>, Eigen::Ref<tinyMatrix>, Eigen::Ref<tinyMatrix>, float, int, int, int, Eigen::Ref<tinyMatrix>, Eigen::Ref<tinyMatrix>, Eigen::Ref<tinyMatrix>, Eigen::Ref<tinyMatrix>, TinySettings*, int>(),
+            "A"_a.noconvert(), "B"_a.noconvert(), "Q"_a.noconvert(), "R"_a.noconvert(), "rho"_a, "nx"_a, "nu"_a, "N"_a, "x_min"_a.noconvert(), "x_max"_a.noconvert(), "u_min"_a.noconvert(), "u_max"_a.noconvert(), "settings"_a, "verbose"_a)
     .def_property_readonly("solution", &PyTinySolver::get_solution)
-    .def("solve", &PyTinySolver::solve);
+    .def("set_x0", &PyTinySolver::set_x0)
+    .def("set_x_ref", &PyTinySolver::set_x_ref)
+    .def("set_u_ref", &PyTinySolver::set_u_ref)
+    .def("solve", &PyTinySolver::solve)
+    .def("print_problem_data", &PyTinySolver::print_problem_data);
 
 }
--- src/tinympc/interface.py
+++ src/tinympc/interface.py
@@ -47,11 +47,29 @@ class TinyMPC:
         if self._solver is not None:
             self._solver.update_settings(self.settings)        
         
-        
+
+    def expand_ndarray(self, array_, expected_rows, expected_cols, fallback):
+        """Takes array_ given by a user, can be of size expected_rows x 1 or expected_rows x expected_cols.
+        If of size expected_rows x 1, expands to be of size expected_rows x expected_cols.
+        If neither, returns array_ of size expected_rows x expected_cols full of the fallback number.
+        """
+        if array_ is not None:
+            assert array_.shape == (expected_rows, expected_cols) or array_.shape == (expected_rows,), "Expected numpy array to have shape ({},{}) or ({},)".format(expected_rows, expected_cols, expected_rows)
+            if len(array_.shape) == 1:
+                if len(array_) == expected_rows: # If expected_rows x 1, expand to expected_rows x expected_cols
+                    array_ = np.array([array_]*expected_cols).T
+            elif len(array_.shape) == 2: # If already expected_rows x expected_cols, do nothing
+                if array_.shape == (expected_rows, expected_cols):
+                    array_ = array_
+            array_[array_ == None] = fallback # Replace all None values with fallback
+            assert array_.shape == (expected_rows, expected_cols)
+        else:
+            array_ = np.ones((expected_rows, expected_cols))*fallback
+        return array_
 
     # Setup the problem data and solver options
     def setup(self, A, B, Q, R, N, rho=1.0,
-        x_min=None, x_max=None, u_min=None, u_max=None, **settings):
+        x_min=None, x_max=None, u_min=None, u_max=None, verbose=False, **settings):
         """Instantiate necessary algorithm variables and parameters
         
         :param A (np.ndarray): State transition matrix of the linear system, size nx x nx
@@ -63,6 +81,7 @@ class TinyMPC:
         :param x_max (list[float] or None, optional): Upper bound state constraints of the same length as nx, default None
         :param u_min (list[float] or None, optional): Lower bound input constraints of the same length as nu, default None
         :param u_max (list[float] or None, optional): Upper bound input constraints of the same length as nu, default None
+        :param verbose (bool): Whether or not to print data to console during setup, default False
         :params settings: Dictionary of optional settings
             :param abs_pri_tol (float): Solution tolerance for primal variables
             :param abs_dua_tol (float): Solution tolerance for dual variables
@@ -71,6 +90,7 @@ class TinyMPC:
             :param en_state_bound (bool): Enable or disable bound constraints on state
             :param en_input_bound (bool): Enable or disable bound constraints on input
         """
+        self.rho = rho
         assert A.shape[0] == A.shape[1]
         assert A.shape[0] == B.shape[0]
         assert Q.shape[0] == Q.shape[1]
@@ -81,22 +101,34 @@ class TinyMPC:
         self.B = np.array(B, order="F")
         self.Q = np.array(Q, order="F")
         self.R = np.array(R, order="F")
-        self.rho = rho
+
         self.nx = A.shape[0]
         self.nu = B.shape[1]
+
+        assert N > 1
         self.N = N
-        self.x_min = x_min if x_min is not None else np.array([-self._infty]*self.nx)
-        self.x_max = x_max if x_max is not None else np.array([self._infty]*self.nx)
-        self.u_min = u_min if u_min is not None else np.array([-self._infty]*self.nu)
-        self.u_max = u_max if u_max is not None else np.array([self._infty]*self.nu)
-        assert len(self.x_min.shape) == 1
-        assert len(self.x_max.shape) == 1
-        assert len(self.u_min.shape) == 1
-        assert len(self.u_max.shape) == 1
-        assert len(self.x_min) == self.nx
-        assert len(self.x_max) == self.nx
-        assert len(self.u_min) == self.nu
-        assert len(self.u_max) == self.nu
+
+
+        self.x_min = np.array(self.expand_ndarray(x_min, self.nx, self.N, -self._infty), dtype=float, order="F")
+        self.x_max = np.array(self.expand_ndarray(x_max, self.nx, self.N, self._infty), dtype=float, order="F")
+        self.u_min = np.array(self.expand_ndarray(u_min, self.nu, self.N-1, -self._infty), dtype=float, order="F")
+        self.u_max = np.array(self.expand_ndarray(u_max, self.nu, self.N-1, self._infty), dtype=float, order="F")
+
+        assert len(self.x_min.shape) == 2
+        assert len(self.x_max.shape) == 2
+        assert len(self.u_min.shape) == 2
+        assert len(self.u_max.shape) == 2
+        assert self.x_min.shape[0] == self.nx
+        assert self.x_max.shape[0] == self.nx
+        assert self.u_min.shape[0] == self.nu
+        assert self.u_max.shape[0] == self.nu
+        assert self.x_min.shape[1] == self.N
+        assert self.x_max.shape[1] == self.N
+        assert self.u_min.shape[1] == self.N-1
+        assert self.u_max.shape[1] == self.N-1
+
+        self.verbose = verbose
+
 
         self.settings = self.ext.TinySettings() # instantiate local settings (settings known only to the python interface)
         self.ext.tiny_set_default_settings(self.settings) # set local settings to default defined by C++ implementation
@@ -105,8 +137,40 @@ class TinyMPC:
         self._solver = self.ext.TinySolver(self.A, self.B, self.Q, self.R, self.rho,
                                            self.nx, self.nu, self.N,
                                            self.x_min, self.x_max, self.u_min, self.u_max,
-                                           self.settings
+                                           self.settings, self.verbose
         )
 
+    def set_x0(self, x0):
+        assert len(x0.shape) == 1
+        assert len(x0) == self.nx
+
+        self._solver.set_x0(x0)
+    
+    def set_x_ref(self, x_ref):
+        """Set state reference trajectory
+        
+        :param x_ref (np.ndarray): State reference trajectory, can be of size nx x 1 or nx x N.
+                If of size nx x 1, expands to be of size nx x N
+        """
+        x_ref = np.array(self.expand_ndarray(x_ref, self.nx, self.N, 0), dtype=float, order="F")
+        self._solver.set_x_ref(x_ref)
+        
+    def set_u_ref(self, u_ref):
+        """Set input reference trajectory
+        
+        :param u_ref (np.ndarray): Input reference trajectory, can be of size nu x 1 or nu x N-1.
+                If of size nu x 1, expands to be of size nu x N-1
+        """
+        u_ref = np.array(self.expand_ndarray(u_ref, self.nu, self.N-1, 0), dtype=float, order="F")
+        self._solver.set_u_ref(u_ref)
+
     def solve(self):
+        # self._solver.print_problem_data()
         self._solver.solve()
+
+        solution = self._solver.solution
+
+        if not solution.solved and self.verbose:
+            print("Problem not solved after {} iterations".format(solution.iter))
+
+        return {"states_all": solution.x.T, "controls_all": solution.u.T, "controls": solution.u[:,0]}
